ddc7ba34a1bb6ca6ab638c35b5536e718b1b9714
Yue Yan <yueyan2@illinois.edu>
2024-09-07 23:06:03 -0500

    solution

--- extreme_edge_cases/camelCaser.c
+++ extreme_edge_cases/camelCaser.c
@@ -4,13 +4,101 @@
  */
 #include "camelCaser.h"
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+char **split(const char *input_str) { //split according to "."
+    char **result = NULL;
+    int idx = 0;
+    int prev_word_end_idx = 0;
+    int number_of_strings = 0;
+    size_t str_length = strlen(input_str);
+    
+    while ((unsigned int)idx < str_length) {
+        if (ispunct(input_str[idx])) {
+            char *temp = malloc(idx - prev_word_end_idx + 1);
+            strncpy(temp, &input_str[prev_word_end_idx], idx - prev_word_end_idx);
+            temp[idx - prev_word_end_idx] = '\0';
+            prev_word_end_idx = idx + 1;
+            number_of_strings += 1;
+
+            size_t size = number_of_strings * sizeof(char*);
+            result = realloc(result, size);
+            result[number_of_strings - 1] = temp;
+        }
+        idx += 1;
+    }
+    number_of_strings += 1;
+    result = realloc(result, number_of_strings * sizeof(char*));
+    result[number_of_strings - 1] = NULL;
+    return result;
+}
 
 char **camel_caser(const char *input_str) {
     // TODO: Implement me!
-    return NULL;
+    char **split_str = split(input_str);
+    int num_of_strings = 0;
+    while (split_str[num_of_strings] != NULL) {
+        num_of_strings += 1;
+    }
+    char **result = malloc((num_of_strings + 1) * sizeof(char*));
+
+    int i = 0;
+    int count = 0; // the index of char * in result
+    while (split_str[i] != NULL) {
+        bool flag = true; // true = start of a new word
+        char *temp = split_str[i];
+        char *new_str = malloc(strlen(temp) + 1);
+        int new_idx = 0;
+        for (int j = 0; (unsigned int)j < strlen(temp); j++) {
+            if (!ispunct(temp[j]) && !isalpha(temp[j]) && !isspace(temp[j])) {
+                new_str[new_idx] = temp[j];
+                new_idx += 1;
+            } else {
+                if (isalpha(temp[j])) {
+                    if (new_idx == 0) { // temp[j] is the first char in this sentence
+                        new_str[new_idx] = tolower(temp[j]);
+                        new_idx += 1;
+                        flag = false;
+                    } else {
+                        if (flag) {
+                            new_str[new_idx] = toupper(temp[j]);
+                            new_idx += 1;
+                            flag = false;
+                        } else {
+                            new_str[new_idx] = tolower(temp[j]);
+                            new_idx += 1;
+                        }
+                    }
+                }
+                if (isspace(temp[j])) {
+                    flag = true;
+                }
+            }
+        }
+        new_str[strlen(temp)] = '\0';
+        result[count] = new_str;
+        count += 1;
+        i += 1;
+    }
+    result[num_of_strings] = NULL;
+    destroy(split_str);
+    return result;
 }
+
 
 void destroy(char **result) {
     // TODO: Implement me!
+    char **temp = result;
+    int i = 0;
+    while (temp[i] != NULL) {
+        char *cur = temp[i];
+        free(cur);
+        i += 1;
+    }
+    free(result);
     return;
 }
+
--- extreme_edge_cases/camelCaser_tests.c
+++ extreme_edge_cases/camelCaser_tests.c
@@ -9,8 +9,85 @@
 #include "camelCaser.h"
 #include "camelCaser_tests.h"
 
+int test(char *input, char **target, char **(*camelCaser)(const char *), void (*destroy)(char **)) {
+    char **output = camelCaser(input);
+    int idx = 0;
+    while (target[idx] != NULL) {
+        if (strcmp(target[idx], output[idx]) != 0) {
+            destroy(output);
+            return 0;
+        }
+        idx += 1;
+    }
+    destroy(output);
+    return 1;
+}
+
+
 int test_camelCaser(char **(*camelCaser)(const char *),
                     void (*destroy)(char **)) {
     // TODO: Implement me!
+    char *input_1 = "";
+    if (!test(input_1, NULL, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_2 = "AAA";
+    if (!test(input_2, NULL, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_3 = "!!!";
+    if (!test(input_3, (char *[]){"", "", "", NULL}, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_4 = "What is this! What is that?";
+    if (!test(input_4, (char *[]){"whatIsThis", "whatIsThat", NULL}, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_5 = "  Leading spaces!   ";
+    if (!test(input_5, (char *[]){"leadingSpaces", NULL}, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_6 = "123 is a number.";
+    if (!test(input_6, (char *[]){"123IsANumber", NULL}, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_7 = "Line1\nLine2\tLine3.";
+    if (!test(input_7, (char *[]){"line1Line2Line3", NULL}, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_8 = "Hello...World!!! What???";
+    if (!test(input_8, (char *[]){"hello",
+        "",
+        "",
+        "world",
+        "",
+        "",
+        "what",
+        "",
+        "",
+        NULL}, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_9 = "    ";
+    if (!test(input_9, NULL, camelCaser, destroy)) {
+        return 0;
+    }
+
+    char *input_10 = "Hello, World ! . ";
+    if (!test(input_10, (char *[]){"hello",
+        "world",
+        "",
+        NULL}, camelCaser, destroy)) {
+        return 0;
+    }
+
     return 1;
 }
