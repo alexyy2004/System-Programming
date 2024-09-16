a976956341dc65a78f2b01a4ad7dc45d2f34272b
Yue Yan <yueyan2@illinois.edu>
2024-09-16 13:09:40 -0500

    strsep with null count

--- vector/sstring_test.c
+++ vector/sstring_test.c
@@ -6,5 +6,20 @@
 
 int main(int argc, char *argv[]) {
     // TODO create some tests
+    sstring *str = cstr_to_sstring("hello world!");
+    char *cstr = sstring_to_cstr(str);
+    printf("%s\n", cstr);
+    // free(cstr);
+    // sstring_destroy(str);
+
+    vector *vec = sstring_split(cstr_to_sstring("abcdeefg"), 'e');
+    
+    for (size_t i = 0; i < vector_size(vec); i++) {
+        char *c = (char *)vector_get(vec, i);
+        printf("%s\n", c);
+    }
+    printf("%zu\n", vector_size(vec));
+    
+    vector_destroy(vec);
     return 0;
 }
