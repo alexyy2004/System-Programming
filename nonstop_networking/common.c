d5ac7fecb66ef271d3e208fd836cbcb28b639672
Yue Yan <yueyan2@illinois.edu>
2024-11-17 23:56:58 -0600

    last

--- nonstop_networking/common.c
+++ nonstop_networking/common.c
@@ -4,46 +4,46 @@
  */
 #include "common.h"
 
-ssize_t read_from_socket(int sockfd, char *buffer, size_t length) {
-    size_t total_bytes_read = 0; // Tracks the total bytes read
-    while (total_bytes_read < length) {
-        ssize_t bytes_read = read(sockfd, buffer + total_bytes_read, length - total_bytes_read);
+// ssize_t read_from_socket(int sockfd, char *buffer, size_t length) {
+//     size_t total_bytes_read = 0; // Tracks the total bytes read
+//     while (total_bytes_read < length) {
+//         ssize_t bytes_read = read(sockfd, buffer + total_bytes_read, length - total_bytes_read);
 
-        if (bytes_read < 0) {
-            if (errno == EINTR) {
-                // Interrupted by a signal, retry the read
-                continue;
-            }
-            perror("read"); // Log the error
-            return -1;      // Return -1 to indicate failure
-        }
+//         if (bytes_read < 0) {
+//             if (errno == EINTR) {
+//                 // Interrupted by a signal, retry the read
+//                 continue;
+//             }
+//             perror("read"); // Log the error
+//             return -1;      // Return -1 to indicate failure
+//         }
 
-        if (bytes_read == 0) {
-            // Connection closed by the peer
-            break;
-        }
+//         if (bytes_read == 0) {
+//             // Connection closed by the peer
+//             break;
+//         }
 
-        total_bytes_read += bytes_read;
-    }
+//         total_bytes_read += bytes_read;
+//     }
 
-    return total_bytes_read; // Return the total bytes read
-}
+//     return total_bytes_read; // Return the total bytes read
+// }
 
-ssize_t write_to_socket(int sockfd, const char *buffer, size_t length) {
-    size_t total_bytes_written = 0;
-    while (total_bytes_written < length) {
-        ssize_t bytes_written = write(sockfd, buffer + total_bytes_written, length - total_bytes_written);
+// ssize_t write_to_socket(int sockfd, const char *buffer, size_t length) {
+//     size_t total_bytes_written = 0;
+//     while (total_bytes_written < length) {
+//         ssize_t bytes_written = write(sockfd, buffer + total_bytes_written, length - total_bytes_written);
 
-        if (bytes_written < 0) {
-            if (errno == EINTR) {
-                continue;
-            }
-            perror("write");
-            return -1;
-        }
+//         if (bytes_written < 0) {
+//             if (errno == EINTR) {
+//                 continue;
+//             }
+//             perror("write");
+//             return -1;
+//         }
 
-        total_bytes_written += bytes_written;
-    }
+//         total_bytes_written += bytes_written;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
