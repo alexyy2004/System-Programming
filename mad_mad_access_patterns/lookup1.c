d34515a26a9eaee5a05dc3f68fd04a6d9c530e86
Yue Yan <yueyan2@illinois.edu>
2024-11-19 19:40:25 -0600

    finish lookup1

--- mad_mad_access_patterns/lookup1.c
+++ mad_mad_access_patterns/lookup1.c
@@ -4,13 +4,72 @@
  */
 #include "tree.h"
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses fseek() and fread() to access the data.
 
   ./lookup1 <data_file> <word> [<word> ...]
 */
+int BinarySearch(FILE* file, char* word, int offset) {
+  if (offset == 0) {
+    return 0;
+  }
+  fseek(file, offset, SEEK_SET);
+  BinaryTreeNode* node = malloc(sizeof(BinaryTreeNode));
+  fread(node, sizeof(BinaryTreeNode), 1, file);
+  int new_offset = offset + sizeof(BinaryTreeNode);
+  fseek(file, new_offset, SEEK_SET);
+  char cur_word[20];
+  fread(cur_word, 20, 1, file);
+  // char cur_word[10];
+  // strcpy(cur_word, node->word);
+  // printf("cur_word is: %s\n", cur_word);
+  // printf("node word is: %s\n", node->word);
+  // printf("word is: %s\n", word);
+  // printf("node count is: %d\n", node->count);
+  if (strcmp(cur_word, word) == 0) {
+    printFound(cur_word, node->count, node->price);
+    free(node);
+    return 1;
+  } else {
+    if (strcmp(word, cur_word) < 0) {
+      int result = BinarySearch(file, word, node->left_child);
+      free(node);
+      return result;
+    } else {
+      int result = BinarySearch(file, word, node->right_child);
+      free(node);
+      return result;
+    }
+  }
+  free(node);
+  return 0;
+}
 
 int main(int argc, char **argv) {
-    return 0;
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL) {
+    openFail(argv[1]);
+    exit(1);
+  }
+
+  char root[4];
+  fread(root, 1, 4, file);
+  if (strcmp(root, "BTRE") != 0) {
+    formatFail(argv[1]);
+    exit(1);
+  }
+
+  for (int i = 2; i < argc; i++) {
+    char* target = argv[i];
+    int find = BinarySearch(file, target, 4);
+    if (!find) {
+      printNotFound(target);
+    }
+    // free(target);
+  }
+  fclose(file);
+  return 0;
 }
